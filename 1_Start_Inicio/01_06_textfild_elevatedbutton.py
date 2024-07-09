cc5aa6386a0d8512b8664e994277059bdd3bd035
juvenalculino <b831381@gmail.com>
2024-07-09 09:19:59 -0300

    Mostrando mensagem na tela

--- 1_Start_Inicio/01_06_textfild_elevatedbutton.py
+++ 1_Start_Inicio/01_06_textfild_elevatedbutton.py
@@ -2,16 +2,24 @@ import flet as ft
 import time
 
 
-def saudacao(e):
-    print('Olá, mundo!')
+def main(page: ft.Page):
+    # Para mostrar o nome na tela precisamos Indicar o objeto textfield dentro de uma variavel
+    txt_nome = ft.TextField(label='Digite seu nome: ')
+
+    lbl_saudar = ft.Text()
     
 
-def main(page: ft.Page):
-    # Capturando nome de uma pessoa com TextFild e ElevatedButton. Os dados setrão mostrados no terminal.
+    def saudar(e):
+        # Podemos passar o value diretamente e utilizar o page.update para mostrar na tela
+        lbl_saudar.value = f'Olá - {txt_nome.value}'
+        page.update()
+
     row = ft.Row(
         controls=[
-            ft.TextField(label='Digite seu nome: '),
-            ft.ElevatedButton(text='Saudação', on_click=saudacao),
+            # Passando o objeto
+            txt_nome,
+            ft.ElevatedButton(text='Saudação', on_click=saudar),
+            lbl_saudar,
         ]
     )
 
